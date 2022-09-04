a8f804741515ddf9354a44223344da0986ce22e4
lara_amaral15 <90003183+LaraAmaral15@users.noreply.github.com>
2022-09-04 19:37:38 -0300

    concluindo o projeto
    concluindo o projeto

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,8 +1,74 @@
-// Input  =========================================
+const texto = document.getElementById('cardsInput')
+const btnInsert = document.querySelector('.inserir button')
+const btnDeleteAll = document.querySelector('.cards-header button')
+const ul = document.getElementById('listaCards')
 
-var captar = "";
+var itensDB = []
 
-function chamar() {
-  captar = document.getElementById('valor').value;
-  document.getElementById('digito').innerHTML = captar;
+btnDeleteAll.onclick = () => {
+  itensDB = []
+  updateDB()
 }
+
+texto.addEventListener('keypress', e => {
+  if (e.key == 'Enter' && texto.value != '') {
+    setItemDB()
+  }
+})
+
+btnInsert.onclick = () => {
+  if (texto.value != '') {
+    setItemDB()
+  }
+}
+
+function setItemDB() {
+
+  itensDB.push({ 'item': texto.value, 'status': '' })
+  updateDB()
+}
+
+function updateDB() {
+  localStorage.setItem('todolist', JSON.stringify(itensDB))
+  loadItens()
+}
+
+function loadItens() {
+  ul.innerHTML = "";
+  itensDB = JSON.parse(localStorage.getItem('todolist')) ?? []
+  itensDB.forEach((item, i) => {
+    insertItemTela(item.item, item.status, i)
+  })
+}
+
+function insertItemTela(text, status, i) {
+  const li = document.createElement('li')
+  
+  li.innerHTML = `
+    <div class="listaCards">
+      <span data-si=${i}>${text}</span>
+      <button onclick="removeItem(${i})" data-i=${i}><i class='bx bx-trash'></i></button>
+    </div>
+    `
+  ul.appendChild(li)
+
+  texto.value = ''
+}
+
+function done(chk, i) {
+
+  if (chk.checked) {
+    itensDB[i].status = 'checked' 
+  } else {
+    itensDB[i].status = '' 
+  }
+
+  updateDB()
+}
+
+function removeItem(i) {
+  itensDB.splice(i, 1)
+  updateDB()
+}
+
+loadItens()
